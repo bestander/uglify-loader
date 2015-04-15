3560c5e396bc404c68d3e2c41ddb8fd236aaf3db
Konstantin Raev <bestander@gmail.com>
2015-04-16 01:34:49 +1200

    source maps work now

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var UglifyJS = require("uglify-js");
+var loaderUtils = require('loader-utils');
 
 module.exports = function(source, inputSourceMap) {
     var callback = this.async();
@@ -8,9 +9,18 @@ module.exports = function(source, inputSourceMap) {
     }
 
     var opts = this.options['uglify-loader'] || {};
-    //    opts.inSourceMap = inputSourceMap;
-    //    opts.outSourceMap = "out.js.map";
+    // just an indicator to generate source maps, the output result.map will be modified anyway
+    opts.outSourceMap = "out.map.js";
     opts.fromString = true;
+
     var result = UglifyJS.minify(source, opts);
-    callback(null, result.code, result.map);
+
+    var sourceFilename = loaderUtils.getRemainingRequest(this);
+    var current = loaderUtils.getCurrentRequest(this);
+    var sourceMap = JSON.parse(result.map);
+    sourceMap.sources = [sourceFilename];
+    sourceMap.file = current;
+    sourceMap.sourcesContent = [source];
+
+    callback(null, result.code, sourceMap);
 };
